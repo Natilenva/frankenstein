02693b211d7206df0d4b9cb7e8a386697cf1a462
Mon Pampín <mon.medialab@gmail.com>
2024-04-22 02:13:12 +0200

    new db and create db only executing initdb.js

--- back/db/initdb.js
+++ back/db/initdb.js
@@ -1,180 +1,118 @@
 import 'dotenv/config';
+console.log('frankenstein variables: ', process.env.MYSQL_HOST, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD);
 
 import { getConnection } from './db.js';
 
 async function main() {
-    let connection;
+    const connection = await getConnection();
+    console.log('Connected to MySQL server');
 
     try {
-        connection = await getConnection();
-        
-        console.log('Poniendo base de datos en uso');
-        await connection.query('USE frankenstein');
-        console.log('Base de datos en uso');
+        // Drop the database if it exists
+        await connection.query(`DROP DATABASE IF EXISTS frankenstein`);
+        console.log('Database dropped if it exists');
 
-        console.log('Borrando tablas existentes');
-        await connection.query
-            ('DROP TABLE IF EXISTS votes_proyects, proyects_comments, proyects, votes_events, events_comments, events, votes_responses, users_responses, responses, questions, topics, users, users_types, persons;'
-            );
-            
-        console.log('Creando tablas');
+        // Create the database
+        await connection.query(`CREATE DATABASE frankenstein`);
+        console.log('Database created successfully');
+    
+        // Selecting the newly created database
+        await connection.query(`USE frankenstein `);
+        console.log(`Using database `);
+
+        // Drop the table if it exists
+        await connection.query('DROP TABLE IF EXISTS register, profile, questions, events, projects, responses, votes');
+        console.log('Tables dropped if it exists');
+
+        console.log('Creating tables');
 
         await connection.query(`
-            CREATE TABLE persons (
-                person_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                name varchar(50) NOT NULL,
-                lastname varchar(50) NOT NULL,
-                birthdate DATETIME NOT NULL,
+            CREATE TABLE register (
+                register_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                email varchar(100) UNIQUE NOT NULL,
+                register_password varchar(100) NOT NULL,
+                register_code varchar(36),
                 created_at datetime DEFAULT CURRENT_TIMESTAMP,
                 modified_at datetime DEFAULT CURRENT_TIMESTAMP
-            );
+            );  
         `);
 
         await connection.query(`
-            CREATE TABLE users_types(
-              user_type_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-              role enum('expert','business', 'students', 'anonymous') DEFAULT  'anonymous'
-            );   
-        `);
-
-        await connection.query(`
-            CREATE TABLE users (
-                user_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                email varchar(100) NOT NULL,
-                password varchar(100) NOT NULL,
-                username varchar(30) DEFAULT NULL,
-                avatar varchar(100) DEFAULT NULL,
-                code_registration varchar(36) DEFAULT NULL,
-                recover_pass_code varchar(36) DEFAULT NULL,
+            CREATE TABLE  profile (
+                profile_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                profile_name varchar(50) NOT NULL,
+                profile_lastname varchar(50) NOT NULL,
+                profile_username varchar(50),
+                birthdate DATETIME,
+                profile_role enum('expert','company', 'students') ,
+                company_name varchar(100) ,
+                avatar varchar(255) ,
                 created_at datetime DEFAULT CURRENT_TIMESTAMP,
                 modified_at datetime DEFAULT CURRENT_TIMESTAMP,
-                person_id  int DEFAULT NULL,
-                FOREIGN KEY (person_id) REFERENCES persons(person_id),
-                user_type_id  int DEFAULT NULL,
-                FOREIGN KEY (user_type_id) REFERENCES users_types(user_type_id)
-            );   
-        `);
-
-        await connection.query(`
-            CREATE TABLE topics (
-              topic_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-              role enum('design_code',' design','code') DEFAULT 'design_code'
-            );  
+                register_id int NOT NULL,
+                FOREIGN KEY (register_id) REFERENCES register(register_id)
+            );
         `);
 
         await connection.query(`
             CREATE TABLE questions (
                 question_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                question_title text NOT NULL,
-                topic_id  int NOT NULL,
-                FOREIGN KEY (topic_id) REFERENCES topics(topic_id),
-                user_id  int NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users(user_id)
-            );
-        `);
-
-        await connection.query(`
-            CREATE TABLE responses (
-              response_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-              response_title text NOT NULL,
-              question_id int NOT NULL,
-              FOREIGN KEY (question_id) REFERENCES questions(question_id),
-              user_id  int NOT NULL,
-              FOREIGN KEY (user_id) REFERENCES users(user_id)
-          );  
-        `);
-
-        await connection.query(`
-                CREATE TABLE users_responses (
-                user_response_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                user_id int NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users(user_id),
-                response_id int NOT NULL,
-                FOREIGN KEY (response_id) REFERENCES responses(response_id)
+                question_title varchar(255)   NOT NULL,
+                question_description text NOT NULL,
+                created_at datetime DEFAULT CURRENT_TIMESTAMP,
+                modified_at datetime DEFAULT CURRENT_TIMESTAMP,
+                register_id  int NOT NULL,
+                FOREIGN KEY (register_id) REFERENCES register(register_id)
             );   
         `);
-
-        await connection.query(`
-            CREATE TABLE votes_responses (
-                vote_response_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                value tinyint NOT NULL,
-                response_id int NOT NULL,
-                FOREIGN KEY (response_id) REFERENCES responses(response_id),
-                user_id  int NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users(user_id)
-            );
-       `);
 
         await connection.query(`
             CREATE TABLE events (
                 event_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                title varchar(100) NOT NULL,
-                place varchar(50) DEFAULT NULL,
-                description text DEFAULT NULL,
-                photo varchar(255) DEFAULT NULL,
+                event_title varchar(100) NOT NULL,
+                event_description text NOT NULL,
+                place varchar(50) NOT NULL,
+                event_url varchar(255),
+                event_photo varchar(255),
                 created_at datetime DEFAULT CURRENT_TIMESTAMP,
                 modified_at datetime DEFAULT CURRENT_TIMESTAMP,
-                user_id int NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users(user_id)
-            );  
-        `);
-
-        await connection.query(`
-            CREATE TABLE events_comments (
-                event_comment_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                comment text,
-                event_id int NOT NULL,
-                FOREIGN KEY (event_id) REFERENCES events(event_id),
-                user_id  int NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users(user_id)
-            );   
-        `);
-
-        await connection.query(`
-            CREATE TABLE votes_events (
-                vote_event_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                value tinyint NOT NULL,
-                event_id int NOT NULL,
-                FOREIGN KEY (event_id) REFERENCES events(event_id),
-                user_id  int NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users(user_id)
+                register_id  int NOT NULL,
+                FOREIGN KEY (register_id) REFERENCES register(register_id)
             );     
         `);
 
         await connection.query(`
             CREATE TABLE projects (
-                  project_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                  title varchar(100) NOT NULL,
-                  description text DEFAULT NULL,
-                  photo varchar(255) DEFAULT NULL,
-                  created_at datetime DEFAULT CURRENT_TIMESTAMP,
-                  modified_at datetime DEFAULT CURRENT_TIMESTAMP,
-                  user_id int DEFAULT NULL,
-                  FOREIGN KEY (user_id) REFERENCES users(user_id)
+                project_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                project_title varchar(100) NOT NULL,
+                project_description text,
+                project_photo varchar(255),
+                project_url varchar(255),
+                created_at datetime DEFAULT CURRENT_TIMESTAMP,
+                modified_at datetime DEFAULT CURRENT_TIMESTAMP,
+                register_id  int NOT NULL,
+                FOREIGN KEY (register_id) REFERENCES register(register_id)
             );
         `);
 
-        await connection.query(` 
-            CREATE TABLE projects_comments (
-                project_comment_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                comment text,
-                project_id int NOT NULL,
-                FOREIGN KEY (project_id) REFERENCES projects(project_id),
-                user_id int NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users(user_id)
+        await connection.query(`
+            CREATE TABLE responses (
+                response_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                response_text text NOT NULL,
+                register_id  int NOT NULL,
+                FOREIGN KEY (register_id) REFERENCES register(register_id)
             );  
         `);
 
         await connection.query(`
-            CREATE TABLE votes_projects (
-                vote_project_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                value tinyint NOT NULL,
-                project_id int NOT NULL,
-                FOREIGN KEY (project_id) REFERENCES projects(project_id),
-                user_id  int NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES users(user_id)
+            CREATE TABLE votes (
+                vote_response_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                vote_value tinyint NOT NULL,
+                register_id  int NOT NULL,
+                FOREIGN KEY (register_id) REFERENCES register(register_id)
             );
         `);
+
     } catch (error) {
         console.error(error);
     } finally {
