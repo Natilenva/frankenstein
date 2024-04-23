0416b6e9ff1cd67afe05ebcbe223d0f83b713248
Antonio Trujillo <antoniotrujillodiaz@gmail.com>
2024-04-23 16:47:40 +0100

    responses votes

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import userRoutes from './usersRoutes.js';
-import proyectRoutes from './projectRouter.js'
+import proyectRoutes from './projectRouter.js';
+import responsesRoutes from './responsesRoutes.js';
 
 const router = express.Router();
 
 router.use(userRoutes);
 router.use(proyectRoutes);
+router.use(responsesRoutes);
 
 export default router;
