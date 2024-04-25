88f37b01fe2b14c783aabbe168564e51d29d257a
Mª Gabriela Hernandez <gabrielahersan988@gmail.com>
2024-04-25 16:43:40 +0200

    Endpoint listado consultas con filtro/búsqueda y ordenación

--- back/src/routes/questionRouter.js
+++ back/src/routes/questionRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import  authUser  from '../middlewares/auth.js';
+import authUser from '../middlewares/auth.js';
 import newQuestionController from '../controllers/questions/newQuestionController.js';
 //import selectQuestionById from '../models/entries/selectQuestionById.js';
 //import questionSelectController from '../controllers/questions/questionSelectController.js';
 import { questionSelectController } from '../controllers/questions/questionSelectController.js';
+import questionController from '../controllers/questions/questionController.js';
 
 const router = express.Router();
 
@@ -13,4 +14,5 @@ router.post('/newquestion', authUser, newQuestionController);
 //router.get('/question/:id', authUser, selectQuestionById);
 router.get('/question/:id', authUser, questionSelectController);
 
+router.get('/getQuestion/:id', questionController);
 export default router;
