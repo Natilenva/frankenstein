02dc9e902fb2a674370a6ae2d5330fff940447f5
Mon Pampín <mon.medialab@gmail.com>
2024-04-28 22:45:34 +0200

    Endpoint listado habilidades/tecnologías, el commit anterior con el mismo nombre debería llamarse Endpoint listado tipologías de consultas

--- back/src/routes/skillRouter.js
+++ back/src/routes/skillRouter.js
@@ -2,14 +2,16 @@ import express from 'express';
 
 import authUser from '../middlewares/auth.js';
 import newExpertSkillController from '../controllers/expert/newExpertSkillController.js';
-//import { selectExpertSkillController } from '../controllers/expert/selectExpertSkillController.js';
+import { selectExpertSkillController } from '../controllers/expert/selectExpertSkillController.js';
 
 const router = express.Router();
+
+// TODO si queremos q solo un user Experto pueda crear una skill faltaría controlarlo
 
 //* Endpoint para crear una nueva habilidad/tecnología/Skill
 router.post('/newexpertskill', authUser, newExpertSkillController);
 
 //* Endpoint listado habilidades/tecnologías
-//router.get('/expertskills', selectExpertSkillController);
+router.get('/expertskills', selectExpertSkillController);
 
 export default router;
