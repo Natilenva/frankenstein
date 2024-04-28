300afbbe87c69d94e453f81edcd888e7efa0b411
Mon Pampín <mon.medialab@gmail.com>
2024-04-28 02:35:00 +0200

    endpoint newcompany with auth and schema

--- back/src/middlewares/auth.js
+++ back/src/middlewares/auth.js
@@ -1,30 +1,25 @@
 import jwt from 'jsonwebtoken';
 import { generateError } from '../helpers/generateError.js';
 const authUser = (req, res, next) => {
-    console.log('req.headers: ', req.headers);
+
     try {
-        // read token from header
         const { authorization } = req.headers;
 
         if (!authorization) {
             throw generateError('Falta cabecera de autorización', 401);
         }
 
-        // verify token
         let token;
         try {
             token = jwt.verify(authorization, process.env.SECRET);
-            console.log('token: ', token);
             req.user = token;
         } catch {
             throw generateError('token no válido', 401);
         }
-
-        //storage token
         req.userId = token.id;
-        console.log('req.userId = token.id: ', req.userId);
 
-        next(); //go to controller
+        next(); 
+
     } catch (error) {
         next(error);
     }
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -2,17 +2,20 @@ import express from 'express';
 //import responseRoutes from './responseRoutes.js';
 import userRoutes from './usersRoutes.js';
 import proyectRoutes from './projectRouter.js';
-
 import questionRoutes from './questionRouter.js';
+import companyRoutes from './companyRouter.js';
+
 
 const router = express.Router();
 
 router.use(userRoutes);
 router.use(proyectRoutes);
 
-
+//* Question Routes 
 router.use(questionRoutes);
 
+//* Company Routes
+router.use(companyRoutes);
 
 
 export default router;
