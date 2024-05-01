3da25ae577be20e851e5958fc6d5d77de4a1943a
Mon Pampín <mon.medialab@gmail.com>
2024-05-01 23:26:09 +0200

    fixed security issue in /response and deleted FK profile_id from responses table

--- back/src/models/responses/insertResponseModel.js
+++ back/src/models/responses/insertResponseModel.js
@@ -4,12 +4,16 @@ import getConnection from '../../db/getConnection.js';
 const insertResponseModel = async (response_text, user_id, question_id) => {
     const connection = await getConnection();
 
+    /* const [response] = await connection.query(
+        `INSERT INTO responses (response_text, profile_id, question_id) VALUES (?, ?, ?)`,
+        [response_text, user_id, question_id]
+    ); */
+
     const [response] = await connection.query(
-        `
-        INSERT INTO responses (response_text, profile_id, question_id) VALUES (?, ?, ?)
-    `,
+        `INSERT INTO responses (response_text, register_id, question_id) VALUES (?, ?, ?)`,
         [response_text, user_id, question_id]
     );
+
 
     return response.insertId;
 };
--- back/src/routes/responsesRoutes.js
+++ back/src/routes/responsesRoutes.js
@@ -1,14 +1,20 @@
 import express from 'express';
+
 import voteResponseController from '../controllers/votes/voteResponseController.js';
 import authUser from '../middlewares/auth.js';
 import deleteResponseController from '../controllers/responses/deleteResponseController.js';
-import insertResponseController2 from '../controllers/responses/insertResponseController2.js';
+//import insertResponseController2 from '../controllers/responses/insertResponseController.js';
+import insertResponseController from '../controllers/responses/insertResponseController.js';
+
 const router = express.Router();
+
 router.post('/response/:response_id/votes', authUser, voteResponseController);
 router.delete('/response/:response_id', authUser, deleteResponseController);
-router.post(
-    '/response/:questionID/:profileID',
-    authUser,
-    insertResponseController2
-);
+
+//* Responder a una pregunta
+//router.post('/response/:questionID/:profileID',authUser,insertResponseController2);
+
+//* Responder a una pregunta SIN Problema de seguridad 
+router.post('/response/:questionID',authUser, insertResponseController);
+
 export default router;
