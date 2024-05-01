826ac921bcab7ce41655c341ec833c5cebae74e8
Valentina Alonso <valentinalonsorodriguez@gmail.com>
2024-05-01 10:49:06 +0200

    endpoints listado de proyectos y consultas por usuario

--- back/src/routes/questionRouter.js
+++ back/src/routes/questionRouter.js
@@ -2,12 +2,11 @@ import express from 'express';
 
 import authUser from '../middlewares/auth.js';
 import newQuestionController from '../controllers/questions/newQuestionController.js';
-//import selectQuestionById from '../models/entries/selectQuestionById.js';
-//import questionSelectController from '../controllers/questions/questionSelectController.js';
+
 import { questionSelectController } from '../controllers/questions/questionSelectController.js';
 import questionController from '../controllers/questions/questionController.js';
 import { questionTechSelectFilterController } from '../controllers/questions/questionTechSelectFilterController.js';
-
+import { getRegisterQuestionsController } from '../controllers/questions/getRegisterQuestionsController.js';
 const router = express.Router();
 
 router.post('/newquestion', authUser, newQuestionController);
@@ -18,5 +17,5 @@ router.get('/getQuestion/:id', questionController);
 
 //* Endpoint listado tipologías de consultas
 router.get('/technologies', questionTechSelectFilterController);
-
+router.get('/register/:id/questions', getRegisterQuestionsController);
 export default router;
