fa3d2790a0f40ee179b7416dadabeb5349f7899b
Valentina Alonso <valentinalonsorodriguez@gmail.com>
2024-05-15 16:57:04 +0200

    gestion perfil usuario

---
+++ front/src/hooks/profilehook/useProfile.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import { getProfileService } from '../../services/profileServices';
+
+export const useProfile = (id) => {
+    const [profile, setProfile] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+    useEffect(() => {
+        const loadProfile = async () => {
+            try {
+                setLoading(true);
+                const data = await getProfileService(id);
+                setProfile(data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        loadProfile();
+    }, [id]);
+    return { profile, loading, error };
+};
