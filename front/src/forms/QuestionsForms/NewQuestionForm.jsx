3ff81eb705eb80e1680ad2b6b090b60684f08137
Valentina Alonso <valentinalonsorodriguez@gmail.com>
2024-05-23 23:11:58 +0200

    newQuestionsForm sin validacion

--- front/src/forms/QuestionsForms/NewQuestionForm.jsx
+++ front/src/forms/QuestionsForms/NewQuestionForm.jsx
@@ -10,30 +10,21 @@ import { toast } from 'react-hot-toast';
 
 import { insertQuestionService } from '../../services/questionService';
 import { AuthContext } from '../../context/AuthContext';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { questionSchema } from '../../../schemas/questionShema';
+
 const NewQuestionForm = () => {
     const navigate = useNavigate();
     const { token } = useContext(AuthContext);
 
-    // const [question_title, setQuestion_title] = useState('');
-    // const [question_description, setQuestion_description] = useState('');
-    // const [technology, setTechnology] = useState('');
+    const [question_title, setQuestion_title] = useState('');
+    const [question_description, setQuestion_description] = useState('');
+    const [technology, setTechnology] = useState('');
 
     const [loading, setLoading] = useState(false);
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        mode: 'onTouched',
-        resolver: zodResolver(questionSchema),
-    });
-    const onSubmit = async (data) => {
+
+    const handleSubmit = async (e) => {
         try {
-            // e.preventDefault();
-            // const formData = new FormData();
+            e.preventDefault();
+            const data = new FormData(e.target);
             await insertQuestionService({ data, token });
 
             setLoading(true);
@@ -49,45 +40,37 @@ const NewQuestionForm = () => {
     };
 
     return (
-        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="question_title">Titulo:</label>
             <input
                 type="text"
                 name="question_title"
                 id="question_title"
-                // value={question_title}
-                // onChange={(e) => setQuestion_title(e.target.value)}
-                {...register('question_title')}
+                value={question_title}
+                onChange={(e) => setQuestion_title(e.target.value)}
+                required
             />
-            <p className="h-4 text-sm text-rose-500">
-                {errors.project_title?.message}
-            </p>
+
             <label htmlFor="technology">Tecnologia:</label>
             <input
                 type="text"
                 name="technology"
                 id="technology"
-                // value={technology}
-                // onChange={(e) => setTechnology(e.target.value)}
-                // required
-                {...register('technology')}
+                value={technology}
+                onChange={(e) => setTechnology(e.target.value)}
+                required
             />
-            <p className="h-4 text-sm text-rose-500">
-                {errors.project_description?.message}
-            </p>
+
             <label htmlFor="question_description">Descripcion:</label>
             <input
                 type="text"
                 name="question_description"
                 id="question_description"
-                // value={question_description}
-                // onChange={(e) => setQuestion_description(e.target.value)}
-                // required
-                {...register('question_description')}
+                value={question_description}
+                onChange={(e) => setQuestion_description(e.target.value)}
+                required
             />
-            <p className="h-4 text-sm text-rose-500">
-                {errors.question_description?.message}
-            </p>
+
             {loading ? (
                 <Loading />
             ) : (
