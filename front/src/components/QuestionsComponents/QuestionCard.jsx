200794ceaa96045888b717cd5fbc6be4be1bc230
Mon Pampín <mon.medialab@gmail.com>
2024-05-29 02:00:17 +0200

    new Tailwind for HomePage, ProjectPost and QuestionCard

--- front/src/components/QuestionsComponents/QuestionCard.jsx
+++ front/src/components/QuestionsComponents/QuestionCard.jsx
@@ -1,38 +1,50 @@
 import PropType from 'prop-types';
-//import moment from 'moment';
 import { Link } from 'react-router-dom';
+
 export const QuestionCard = ({ question }) => {
-    //console.log('PersonQuestionCard, question: ', question);
+
+    {/* NO USAR truncateTextPalabras pq puede hacer q la Card se vea más corta o más larga en comparación a otras Card */} 
+
+    // Función para limitar el número de palabras y añadir puntos suspensivos
+    /* const truncateTextPalabras = (text, limit) => {
+        const words = text.split(' ');
+        if (words.length > limit) {
+            return words.slice(0, limit).join(' ') + '...';
+        }
+        return text;
+    }; */
+
+    // Función para limitar el número de caracteres y añadir puntos suspensivos
+    const truncateTextCaracteres = (text, limit) => {
+        if (text.length > limit) {
+            return text.slice(0, limit) + '...';
+        }
+        return text;
+    };
+
     return (
-        <article className="flex pt-4 pb-4"> {/* p-4 gap-4  */}
+        <article className="flex "> {/* p-4 gap-4  */}
             
-            <div className="shrink-0 ">
-                {/* <img 
-                    className="h-12 w-12 rounded-full" 
-                    src="https://source.unsplash.com/random/face" 
-                    alt="img"
-                ></img>  */}
-                ❓
-            </div>
+            <div>{/* ❓ */}</div>
 
             <Link to={`/question/${question.question_id}`} >
-            <div>
-                <h1 className=" text-lg font-bold leading-tight text-white"> {question.question_title}</h1>             
-                <p className="text-sm font-medium mt-2 text-neutral-200">{ question.question_description}</p>
-                <p className="text-xs font-normal text-neutral-500">Tecnologia: { question.technology}</p>
-                <time className="text-xs text-neutral-500">           
-                    {/* {moment(question.createdAt).format(
-                        'DD/MM/YYYY [a las] HH:mm'
-                    )} */}
-                    {new Date(question.created_at).toLocaleDateString()}
-                </time>
-            </div>
+                <div>
+                    <p className="text-xs font-normal mt-1 text-neutral-500">Tecnologia: { question.technology}</p>
+                    <h1 className=" text-base mt-1 font-medium leading-tight text-white"> {question.question_title}{/* ❓ */}</h1>             
+                    <p className="text-sm font-light mt-2 text-neutral-200">
+                        {truncateTextCaracteres(question.question_description, 55)} {/* Limitar a 100 caracteres */}
+                        {/* NO USAR el truncateTextPalabras pq puede hacer q la Card se estire o acorte*/}  
+                        {/* {truncateTextPalabras(project.project_description, 20)} {/* Limitar a 20 palabras */}
+                        <time className="text-xs font-normal mt-1 text-neutral-500">           
+                            {' '} {' '} {new Date(question.created_at).toLocaleDateString()}
+                        </time>
+                    </p>
+                </div>
             </Link>
 
         </article> 
     );
 };
-/* <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"> */
 QuestionCard.propTypes = {
     question: PropType.object.isRequired,
 };
